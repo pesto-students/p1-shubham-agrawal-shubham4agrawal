4234187862296fe67a9714df3b423b8544d49ccf
shubham4agrawal <sa.shubham007@gmail.com>
2022-04-23 17:44:43 +0530

    pushed assignment 4.2

---
+++ Week-4/assignment-4.2.js
@@ -0,0 +1,18 @@
+var Person = function(){};
+
+Person.prototype.initialize = function(name,age){
+    this.name=name;
+    this.age=age;
+ }
+  
+var Teacher = function(){};
+
+Teacher.prototype.teach = function(subject) {
+    return `${this.name} is now teaching ${subject}`;
+}
+
+Object.setPrototypeOf(Teacher.prototype, Person.prototype);
+ 
+var him = new Teacher();
+him.initialize("Adam",45);
+var p = him.teach("Inheritance");
